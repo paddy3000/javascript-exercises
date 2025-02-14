3f07237cb28b96a5d96d1235a8cbd75574a0768f
paddy3000 <paddyohara1998@hotmail.co.uk>
2025-02-14 11:55:58 +0000

    exercise 3 started

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayIn, subset) {
+  return arrayIn.filter((string) => {!(subset.includes(string))})
 };
 
 // Do not edit below this line
