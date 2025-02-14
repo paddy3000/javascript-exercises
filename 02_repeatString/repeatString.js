09a546dd61255bc1ef8f61106f944139d8486a41
paddy3000 <paddyohara1998@hotmail.co.uk>
2025-02-14 11:32:01 +0000

    exercise 2 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+    if (times<0) {
+        return "ERROR";
+    } else {
+    string="";
+    for (let index = 1; index <= times; index++) {
+        string=string+word;
+        
+    }
+}
 
+    return string;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
