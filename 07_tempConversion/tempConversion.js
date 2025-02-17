08046a920de993ec883f24ef5cced374f698cf81
paddy3000 <paddyohara1998@hotmail.co.uk>
2025-02-17 16:15:15 +0000

    exercise 7 completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celcius=(fahrenheit-32)*5/9;
+
+  if (!Number.isInteger(celcius)) {
+    celcius=Math.round(celcius*10)/10;
+  }
+
+  return celcius;  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let fahrenheit=celcius*9/5+32;
+
+  if (!Number.isInteger(fahrenheit)) {
+    fahrenheit=Math.round(fahrenheit*10)/10;
+  }
+
+  return fahrenheit;
 };
 
 // Do not edit below this line
